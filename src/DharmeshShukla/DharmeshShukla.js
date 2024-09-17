7b998d59c0ffb8a17c098020372409087c49d242
Dharmesh49 <shukladharmesh12@yahoo.com>
2024-09-17 22:34:05 +0530

    updated all file resume

---
+++ src/DharmeshShukla/DharmeshShukla.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import './Dharmesh.css'
+
+function DharmeshShukla() {
+  return (
+    <div class="center-container">     
+    <div className='container  DharmeshBorder'>
+   
+      
+      <h2>Dharmesh Shukla</h2>
+
+      <p>Full Stack Developer</p>
+
+      <p>Mumbai , Kalyan</p>
+      <p>421306</p>
+      
+
+      <br></br>
+
+      <h3>Skills</h3>
+
+      <p>HTML , CSS , JAVASCRIPT , REACT JS , POSTGRESQL ,MYSQL, SQL , NODEJS , TAILWIND CSS , PYTHON , SEMANTIC UI , GITS, </p>
+
+      <h3>Education</h3>
+
+      <p>Arunodya University, 2021
+      MASTER IN COMPUTER APPLICATION
+      GPD 8.4/10
+      </p>
+<br></br>
+      <p>Mumbai University , Mumbai, 2019
+      BACHELOR COMPUTER SCIENCE
+      GPD 8/10
+      </p>
+
+      <h3>Experience</h3>
+
+      <p>Software developer</p>
+      <p className='company'>Multiplexer Solution pvt ltd , Present 2022-2024</p>
+
+      <p>Full Stack Developer </p>
+      <p className='company'>WebCreation private ltd ,      2021-2022</p>
+
+      <p>Web Developer  </p>
+      <p className='company'>I Future Technologies private ltd ,     2021 - 6 month</p>
+
+
+      <h3>Across Internet</h3>
+      <p>https://github.com/Dharmesh49   </p>
+
+     
+
+    </div>
+    </div>
+  )
+}
+
+export default DharmeshShukla
